4844d1a2aa285d1659312c10b0c5055cf3d49e27
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-04-10 15:20:08 +1000

    Update pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.scss'
 
 function Soon({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <title> Soon... </title>
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default Soon
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,22 @@ const Index: NextPage = () => {
   return (
     <>
       <div className='justify-center items-center flex middle text-center'>
-        <div className='lg:grid lg:text-center lg:relative mb-2 text-center gap-3'>
-          <div />
+        <div className='lg:grid lg:text-center lg:relative mb-5 text-center gap-4'>
           <h1>Coming Soon!</h1>
           <h2>This is currently a Work In Progress! Come back later!</h2>
         </div>
       </div>
 
       <div style={{ bottom: 0, left: 0 }}>
-        <p className='text-center' style={{ fontSize:' 1.2rem' }}>
-          Copyright &copy; 2022 - Present | <a href='https://github.com/ArtieFuzzz'> ArtieFuzzz </a> | (<a href='https://www.pixiv.net/en/artworks/90163114'> Background Source </a>) </p>
+        <p className='text-center' style={{ fontSize: ' 1.2rem' }}>
+          Copyright &copy; 2022 - Present |{' '}
+          <a href='https://github.com/ArtieFuzzz'> ArtieFuzzz </a> | (
+          <a href='https://www.pixiv.net/en/artworks/90163114'>
+            {' '}
+            Background Source{' '}
+          </a>
+          ){' '}
+        </p>
       </div>
     </>
   )
