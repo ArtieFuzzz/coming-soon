f70e66ab83ae509dc1246f0734186aaa21844e5a
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-04-10 14:16:44 +1000

    Get stuff working

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <div className='justify-center items-center flex middle-full text-center'>
+        <div className='lg:grid lg:text-center lg:relative mb-2 text-center gap-3'>
+          <div />
+          <h1>Coming Soon!</h1>
+          <h2>This is currently a Work In Progress! Come back later!</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Index
